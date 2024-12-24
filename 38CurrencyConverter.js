a680eded1a2b17b2fbec6bb9c26d2322ff78cb54
mrrayyan1801Rayyan123 <mrrayyan1801@gmail.com>
2024-12-24 08:44:36 +0530

    currency converter

---
+++ 38CurrencyConverter.js
@@ -0,0 +1,26 @@
+document.getElementById("calculate").addEventListener("click", () => {
+ 
+
+let fromCurrency=document.getElementById("fromCurrency").value;
+let toCurrency=document.getElementById("toCurrency").value;
+let amount=document.getElementById("amount").value;
+
+console.log(fromCurrency)
+console.log(toCurrency)
+console.log(amount)
+
+fetch("https://api.exchangerate-api.com/v4/latest/"+fromCurrency)
+    .then(data => data.json())
+    .then((data) => {
+
+        const h1=document.createElement("h1")
+        h1.appendChild(document.createTextNode("Answer is "+(data["rates"][toCurrency]*amount)))
+        document.body.appendChild(h1);
+     
+    });
+
+
+  
+});
+
+
